a758114fcc3b0939d5ba366657e28ee7f4b909eb
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-08-24 10:13:06 -0300

    radix initialized

---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,8 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import styled from 'styled-components'
+
+export const Overlay = styled(Dialog.Overlay)`
+
+`
+
+export const Content = styled(Dialog.Content)``
