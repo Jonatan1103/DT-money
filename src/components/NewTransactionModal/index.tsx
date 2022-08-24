7437c199f79e9edd40ca827326d08789d2e6d128
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-08-24 10:53:40 -0300

    modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,15 +1,28 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { Overlay } from './styles'
+import { X } from 'phosphor-react'
+import { CloseButton, Content, Overlay } from './styles'
 
 export function NewTransactionModal() {
   return (
     <Dialog.Portal>
       <Overlay />
 
-      <Dialog.Content>
+      <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
-        <Dialog.Close />
-      </Dialog.Content>
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder='Descrição' required />
+          <input type="number" placeholder='Preço' required />
+          <input type="text" placeholder='Categoria' required />
+
+          <button type='submit'>
+            Cadastrar
+          </button>
+        </form>
+      </Content>
 
     </Dialog.Portal>
   )
