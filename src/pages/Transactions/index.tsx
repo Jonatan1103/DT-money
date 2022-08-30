03190d3fed0fa43f281876d66740837ffd9cadc5
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-08-30 13:05:32 -0300

    performance react

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContextSelector } from "use-context-selector";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
@@ -8,7 +8,10 @@ import { PriceHightLight, TransactionContainer, TransactionsTable } from "./styl
 
 
 export function Transaction() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, 
+    (context) => {
+      return context.transactions
+    })
 
   return (
     <div>
